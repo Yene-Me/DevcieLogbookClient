abb902e8eb18961eb5540b07e58d68dc7996fc91
Lewis Quaife <lewis.quaife@playtech.com>
2016-11-07 10:48:01 +0000

    fixed untracked files

--- build/src/app/deviceRecord/device-log-view.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var DeviceLogView = (function () {
-    function DeviceLogView() {
-    }
-    return DeviceLogView;
-}());
-exports.DeviceLogView = DeviceLogView;
-//# sourceMappingURL=device-log-view.js.map
