e6486b90c45f114dabef4df5bb944519861cd26c
yeneneh.mulatu <myeneneh@hotmail.com>
2016-11-07 17:35:00 +0000

    adding user name

--- src/app/devices/listDevice/devices.component.ts
+++ src/app/devices/listDevice/devices.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {DeviceLog} from '../../deviceRecord/device-log.component'
 import {AngularFire, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2';
+import {User} from '../../auth/user/user'
 
 import '../../../../public/css/styles.css';
 import '../../../../public/css/bootstrap.css';
@@ -12,17 +13,35 @@ import '../../../../public/css/bootstrap.css';
 export class DevicesComponent implements OnInit {
     name:string = "Camden";
     deviceLog:DeviceLog;
-    userId : string
+    userId:string;
+    deviceView:any;
 
     ngOnInit():void {
     }
 
     devices:FirebaseListObservable<any[]>;
+
     constructor(af:AngularFire) {
         this.devices = af.database.list('/devices');
+        this.deviceView = [];
 
-        this.devices.subscribe(data => {
-           console.log("data", data);
+        this.devices.subscribe((deviceData:any) => {
+            this.deviceView = [];
+            for (let item in deviceData) {
+                var userId = deviceData[item].userId;
+                if (userId) {
+                    var user = af.database.object('/users/' + userId);
+                    user.subscribe((data:User) => {
+
+                        deviceData[item].inUseBy = data.displayName;
+                    })
+                }
+                else {
+                    deviceData[item].inUseBy = "";
+                }
+                console.log("deviceData", deviceData);
+                this.deviceView.push(deviceData[item]);
+            }
         });
 
         this.deviceLog = new DeviceLog(af);
@@ -33,22 +52,19 @@ export class DevicesComponent implements OnInit {
 
     //update device log as return
     onReturn(device:any):void {
-        if(this.userId != device.userId)
-        {
+        if (this.userId != device.userId) {
             alert("have really borrowed this device :)");
         }
-        else
-        {
+        else {
             this.deviceLog.onSave(device, this.userId, "in");
-            this.devices.update(device,{userId:""});
+            this.devices.update(device, {userId: ""});
         }
-
 
     }
 
     //update device log as borrowed
     onBorrow(device:any):void {
         this.deviceLog.onSave(device, this.userId, "out");
-        this.devices.update(device,{userId:this.userId})
+        this.devices.update(device, {userId: this.userId})
     }
 }
