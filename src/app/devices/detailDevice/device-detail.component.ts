481a7ac44ab7d19e286a88c17f7061502862f259
yeneneh.mulatu <myeneneh@hotmail.com>
2016-11-08 15:07:36 +0000

    list of logs

--- src/app/devices/detailDevice/device-detail.component.ts
+++ src/app/devices/detailDevice/device-detail.component.ts
@@ -1,51 +1,48 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable }     from 'rxjs/Observable';
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Observable}     from 'rxjs/Observable';
 import {AngularFire, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2';
 import 'rxjs/add/operator/map';
 @Component({
-  selector: 'my-device-detail',
-  templateUrl: './device-details.template.html'
+    selector: 'my-device-detail',
+    templateUrl: './device-details.template.html'
 })
-export class DeviceDetailComponent  implements OnInit {
-  @Input()
-  device:  FirebaseObjectObservable<any>;
-  deviceLog:FirebaseListObservable<any>;
-  sub:any;
-  sessionId: Observable<string>;
-  token: Observable<string>;
-  deviceView:any;
+export class DeviceDetailComponent implements OnInit {
+    @Input()
+    device:FirebaseObjectObservable<any>;
+    deviceLog:FirebaseListObservable<any>;
+    sub:any;
+    sessionId:Observable<string>;
+    token:Observable<string>;
+    deviceView:any;
+    deviceLogView:any;
 
-  constructor(private route: ActivatedRoute, private af: AngularFire)
-  {
+    constructor(private route:ActivatedRoute, private af:AngularFire) {
 
-  }
+    }
 
-  ngOnInit()
-  {
-    this.deviceView = [];
-    this.sub = this.route.queryParams.subscribe(params => {
-      this.getDeviceById(params['device_id']);
-      this.getDeviceLogById(params['device_id']);
-    });
+    ngOnInit() {
+        this.deviceView = [];
+        this.sub = this.route.queryParams.subscribe(params => {
+            this.getDeviceById(params['device_id']);
+            this.getDeviceLogById(params['device_id']);
+        });
 
-  }
+    }
 
-  getDeviceById(id:string)
-  {
-    this.device = this.af.database.object('/devices/'+id);
-    this.device.subscribe((deviceData:any) => {
-      this.deviceView = deviceData;
-    });
-  }
+    getDeviceById(id:string) {
+        this.device = this.af.database.object('/devices/' + id);
+        this.device.subscribe((deviceData:any) => {
+            this.deviceView = deviceData;
+        });
+    }
 
-  getDeviceLogById(id:string)
-  {
-    this.deviceLog = this.af.database.list('/devicesLogs/'+id);
-    this.deviceLog.subscribe((deviceLogData:any) => {
-      console.log("deviceLogData", deviceLogData);
+    getDeviceLogById(id:string) {
+        this.deviceLog = this.af.database.list('/devicesLogs/' + id);
 
-    });
-  }
+        this.deviceLog.subscribe((deviceLogData:any) => {
+
+        });
+    }
 
 }
