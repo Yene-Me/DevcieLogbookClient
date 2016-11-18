095cd0efc357ae6e0a637062c261e750707507b7
yeneneh.mulatu <myeneneh@hotmail.com>
2016-11-18 15:58:16 +0000

    trying cordova

---
+++ src/app/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "phonegap-nfc.NFC",
+        "file": "plugins/phonegap-nfc/www/phonegap-nfc.js",
+        "pluginId": "phonegap-nfc",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.0",
+    "phonegap-nfc": "0.6.6"
+};
+// BOTTOM OF METADATA
+});
