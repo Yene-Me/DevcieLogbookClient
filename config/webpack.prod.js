176779923027258ed85537b64a689a6c047c061f
yeneneh.mulatu <myeneneh@hotmail.com>
2017-01-03 14:32:34 +0000

    update package and webpack prod

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -23,11 +23,7 @@ module.exports = webpackMerge(commonConfig, {
   plugins: [
     new webpack.NoErrorsPlugin(),
     new webpack.optimize.DedupePlugin(),
-  /*  new webpack.optimize.UglifyJsPlugin({ // https://github.com/angular/angular/issues/10618
-      mangle: {
-        keep_fnames: fasle
-      }
-    }),*/
+    //new webpack.optimize.UglifyJsPlugin(/*https://github.com/angular/angular/issues/10618*/),
     new ExtractTextPlugin('[name].[hash].css'),
     new webpack.DefinePlugin({
       'process.env': {
